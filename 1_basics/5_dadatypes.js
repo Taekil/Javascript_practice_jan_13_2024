b321c8706108d1f719211dfa40a835ea0c52b14e
tk <dadakil83@gmail.com>
2024-01-14 13:43:27 -0800

    concept added data types

--- 1_basics/5_dadatypes.js
+++ 1_basics/5_dadatypes.js
@@ -3,4 +3,18 @@
  * 
  * 6 EA Primitive Types and 1 EA Object type
  * 
+ *  1) Number
+ *  2) String
+ *  3) Boolean
+ *  4) Undefined
+ *  5) null
+ *  6) Symbol: built-in object, symbol() call is guranteed to return
+ * a unique symbol.  
+ *  
+ *  7) Object: storeing various keyed collections and more complex
+ * entities.
+ *      ex) object() constructor 
  */
+
+
+
