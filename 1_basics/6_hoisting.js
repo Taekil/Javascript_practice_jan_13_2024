b9ccdb41f49bd08272739da206fec3db11e24d74
tk <dadakil83@gmail.com>
2024-01-20 21:17:45 -0800

    new operator tutorial

---
+++ 1_basics/6_hoisting.js
@@ -0,0 +1,27 @@
+/**
+ * Hoisting
+ */
+
+console.log('Hello');
+console.log('World');
+
+// console.log(name);
+// var name = 'Code_Factory';
+// console.log(name);
+
+/**
+ * Hoisting? 
+ * all var - looks like tranferring to the top of code
+ */
+// var name;
+// console.log(name);
+// name = 'Code_Factory';
+// console.log(name);
+
+// console.log(yuJin); // error, cannot access
+// let yuJin = 'Ann';
+
+// var cannot protect from Hoisting. 
+// So using let or const
+
+ 
